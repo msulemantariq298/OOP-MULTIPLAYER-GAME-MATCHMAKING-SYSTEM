948f8fa09ddfcdde5fd4b2eef1ba6d4c4b5d2a1d
Muhammad Suleman Tariq <msulemantariq298.com>
2026-04-18 10:44:48 +0500

    Update game mode classes
    - Enhanced GameMode with additional configuration options
    - Improved CasualMode implementation with quick match functionality

--- model/CasualMode.java
+++ model/CasualMode.java
@@ -1,5 +1,20 @@
 package model;
 
-public class CasualMode {
+public class CasualMode extends GameMode {
+    private boolean quickMatchEnabled;
     
+    public CasualMode() {
+        super("Casual", 1, 10);
+        this.quickMatchEnabled = true;
+    }
+    
+    @Override
+    public boolean applyMatchRules(Player... players) {
+        // Implementation for casual match rules
+        return true;
+    }
+    
+    public void enableQuickMatch() {
+        this.quickMatchEnabled = true;
+    }
 }
--- model/GameMode.java
+++ model/GameMode.java
@@ -1,5 +1,36 @@
 package model;
 
-public class GameMode {
-    
+public abstract class GameMode {
+    private String modeName;
+    private int minPlayers;
+    private int maxPlayers;
+    private double skillTolerance;
+    private boolean ratingEnabled;
+
+
+    public GameMode(String name, int minP, int maxP) {
+        this.modeName = name;
+        this.minPlayers = minP;
+        this.maxPlayers = maxP;
+        this.skillTolerance = 100;
+        this.ratingEnabled = true;
+    }
+        
+    public String getModeName() {
+        return modeName;
+    }
+        
+    public double getSkillTolerance() {
+        return skillTolerance;
+    }
+        
+    public boolean isRatingEnabled() {
+        return ratingEnabled;
+    }
+        
+    public abstract boolean applyMatchRules(Player... players);
+
+    public String toString() {
+        return modeName;
+    }
 }
