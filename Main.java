0b763ecb220d1fe576e84043780d8bf0498d5bd8
Noor <noorulhuda105432@gmail.com>
2026-04-18 20:47:59 +0500

    changes made in main .java

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
 import ui.ConsoleUI;
 
 public class Main {
-    public static void main(String[] args)
+    public static void main(String[] args) throws InterruptedException {
         ConsoleUI ui = new ConsoleUI();
         ui.start();
-    
+    }
 }
